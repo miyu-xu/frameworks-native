5fae107f80824bf6c4c8a020ff669fc1faeb6a6b
Kevin Lindkvist <lindkvist@google.com>
2022-11-23 13:23:42 +0100

    Enable compilation of libbinder on Fuchsia
    This ensures that getMinSchedulerPolicy and getMinSchedulerPriority are
    only called on Linux as they are only defined here.
    
    This replaces some __ANDROID__ ifdef's with BINDER_WITH_KERNEL_IPC.
    
    Change-Id: Idec83bf1715ac3c62ac7a849aa188aa491a19fda

--- libs/binder/ProcessState.cpp
+++ libs/binder/ProcessState.cpp
@@ -613,7 +613,7 @@ ProcessState::ProcessState(const char* driver)
         }
     }
 
-#ifdef __ANDROID__
+#if defined(BINDER_WITH_KERNEL_IPC)
     LOG_ALWAYS_FATAL_IF(!opened.ok(),
                         "Binder driver '%s' could not be opened. Error: %s. Terminating.",
                         driver, error.c_str());
