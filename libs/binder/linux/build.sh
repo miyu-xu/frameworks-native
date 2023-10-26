0d44d02631ed16a97c7f4bec4c18f8c88b1af9ce
Hao Chen <chenhaosjtuacm@google.com>
2023-10-26 00:03:13 +0000

    [DO NOT SUBMIT] Test of cc_cmake_snapshot
    Test: cd frameworks/native/libs/binder/linux
          ./build.sh
    Change-Id: Id5097300e6f88fab58949d0e859e785790c0391b

---
+++ libs/binder/linux/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# cc_cmake_snapshot with include_sources pulls all source files, including *.zip and build/ in this directory
+git clean -fdx
+
+${ANDROID_BUILD_TOP}/build/soong/soong_ui.bash --make-mode binder_sdk
+cp ${ANDROID_BUILD_TOP}/out/soong/.intermediates/frameworks/native/libs/binder/binder_sdk/*/*/binder_sdk.zip ./binder_sdk.zip
+unzip -q -o binder_sdk.zip -d build
+
+cmake \
+  -G Ninja \
+  -DANDROID_BUILD_TOP=$ANDROID_BUILD_TOP \
+  -B build \
+  build
+cmake --build build
+
+if [ "$DO_TEST" == "1" ]; then
+  cd build/frameworks/native/libs/binder/tests/
+  ./binderRpcTestNoKernel
+fi
