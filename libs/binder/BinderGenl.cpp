9950fcff778115d3c175b19e75466349979612fe
Li Li <dualli@google.com>
2024-01-24 17:25:35 -0800

    add binder genl report support
    This adds a class BinderGenl to talk to the kernel binder genl driver.
    It allows AMS to listen to the kernel binder genl driver for certain
    kinds of abnormal binder transactions and take corresponding actions.
    
    The following binder tests are added to test the kernel driver:
    
    * GenlOpen: open and close the binder generic netlink socket
    * GenlMonitor: read the binder generic netlink socket
    * GenlXxxReply: test various kinds of transactions
    
    Bug: 332610283
    Test: boot Pixel with ACK 6.6 and check logcat
    Test: atest binderLibTest
    Change-Id: I485f8e7c0c0bd7b4c0ccc22eaaad44dd277a22a6

---
+++ libs/binder/BinderGenl.cpp
@@ -0,0 +1,314 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "BinderGenl"
+
+#define LOG_NDEBUG 0
+
+#include <binder/BinderGenl.h>
+#include <errno.h>
+#include <log/log.h>
+#include <string.h>
+#include <unistd.h>
+
+namespace android {
+
+int BinderGenl::open() {
+    int ret;
+    struct sockaddr_nl sa;
+    ALOGV("Opening binder genl socket");
+    ret = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
+    if (ret < 0) {
+        ALOGE("Failed to open binder genl socket: %s", strerror(ret));
+        return ret;
+    }
+    fd = ret;
+
+    pid = (__u32)getpid();
+    memset(&sa, 0, sizeof(sa));
+    sa.nl_family = AF_NETLINK;
+    sa.nl_pid = pid;
+    ALOGV("Binding binder genl socket %d", fd);
+    ret = bind(fd, (struct sockaddr*) &sa, sizeof(sa));
+    if (ret < 0) {
+        ALOGE("Failed to bind binder genl socket: %s", strerror(ret));
+        close();
+        return -1;
+    }
+
+    if (getFamilyId() < 0) {
+        ALOGE("Failed to get binder genl family id");
+        close();
+        return -1;
+    }
+
+    ALOGD("[%s]: genl socket[%d]: %d", name, pid, fd);
+
+    return 0;
+}
+
+void BinderGenl::close() {
+    if (fd > 0) {
+        ALOGV("Closing binder genl socket %d", fd);
+        ::close(fd);
+        fd = 0;
+    }
+}
+
+int BinderGenl::send(__u16 nlmsgType, __u8 cmd, __u16 nlaType, void *nlaData,
+                     __u16 nlaLen) {
+    struct GenlMsg msg;
+    struct nlattr *nla;
+    struct sockaddr_nl sa;
+    int ret;
+
+    if (nlaLen > BINDER_GENL_MSG_SIZE - NLA_HDRLEN - 1) {
+        ALOGE("Oversized binder genl data to send");
+        return -ENOMEM;
+    }
+
+    msg.nlh.nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN);
+    msg.nlh.nlmsg_type = nlmsgType;
+    msg.nlh.nlmsg_flags = NLM_F_REQUEST;
+    msg.nlh.nlmsg_seq = 0;
+    msg.nlh.nlmsg_pid = pid;
+    msg.glh.cmd = cmd;
+    msg.glh.version = BINDER_GENL_FAMILY_VERSION;
+    nla = (struct nlattr*) GENLMSG_DATA(&msg.glh);
+    nla->nla_type = nlaType;
+    nla->nla_len = nlaLen + NLA_HDRLEN;
+    memcpy(NLA_DATA(nla), nlaData, nlaLen);
+    msg.nlh.nlmsg_len += NLMSG_ALIGN(nla->nla_len);
+    memset(&sa, 0, sizeof(sa));
+    sa.nl_family = AF_NETLINK;
+    ret = sendto(fd, &msg, msg.nlh.nlmsg_len, 0, (struct sockaddr *)&sa, sizeof(sa));
+    if (ret < 0) {
+        ALOGE("Failed to send (%d %d %d %d) to binder genl: %s", nlmsgType, cmd,
+              nlaType, nlaLen, strerror(errno));
+    }
+    ALOGV("Sent %d / %d bytes to binder genl", ret, msg.nlh.nlmsg_len);
+
+    return ret;
+}
+
+int BinderGenl::send2(__u16 nlmsgType, __u8 cmd, __u16 nlaType, void *nlaData,
+                      __u16 nlaLen, __u16 nlaType2, void *nlaData2, __u16 nlaLen2) {
+    struct GenlMsg msg;
+    struct nlattr *nla, *nla2;
+    struct sockaddr_nl sa;
+    int ret;
+
+    if (nlaLen + nlaLen2 > BINDER_GENL_MSG_SIZE - NLA_HDRLEN - 1) {
+        ALOGE("Oversized binder genl data to send");
+        return -ENOMEM;
+    }
+
+    msg.nlh.nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN);
+    msg.nlh.nlmsg_type = nlmsgType;
+    msg.nlh.nlmsg_flags = NLM_F_REQUEST;
+    msg.nlh.nlmsg_seq = 0;
+    msg.nlh.nlmsg_pid = pid;
+    msg.glh.cmd = cmd;
+    msg.glh.version = BINDER_GENL_FAMILY_VERSION;
+    nla = (struct nlattr*) GENLMSG_DATA(&msg.glh);
+    nla->nla_type = nlaType;
+    nla->nla_len = nlaLen + NLA_HDRLEN;
+    memcpy(NLA_DATA(nla), nlaData, nlaLen);
+    msg.nlh.nlmsg_len += NLMSG_ALIGN(nla->nla_len);
+    nla2 = (struct nlattr*)((char *)nla + NLA_ALIGN(nla->nla_len));
+    nla2->nla_type = nlaType2;
+    nla2->nla_len = nlaLen2 + NLA_HDRLEN;
+    memcpy(NLA_DATA(nla2), nlaData2, nlaLen2);
+    msg.nlh.nlmsg_len += NLMSG_ALIGN(nla2->nla_len);
+    memset(&sa, 0, sizeof(sa));
+    sa.nl_family = AF_NETLINK;
+    ret = sendto(fd, &msg, msg.nlh.nlmsg_len, 0, (struct sockaddr *)&sa, sizeof(sa));
+    if (ret < 0) {
+        ALOGE("Failed to send (%d %d %d %d %d %d) to binder genl: %s", nlmsgType, cmd,
+              nlaType, nlaLen, nlaType2, nlaLen2, strerror(errno));
+    }
+    ALOGV("Sent %d / %d bytes to binder genl", ret, msg.nlh.nlmsg_len);
+
+    return ret;
+}
+
+int BinderGenl::recv(struct GenlMsg* msg, int len) {
+    int ret;
+
+    ret = ::recv(fd, msg, len, 0);
+    ALOGV("Recv %d from binder genl", ret);
+    ALOGV("nlh: %d %d %d %d %d",
+          msg->nlh.nlmsg_len,
+          msg->nlh.nlmsg_type,
+          msg->nlh.nlmsg_flags,
+          msg->nlh.nlmsg_seq,
+          msg->nlh.nlmsg_pid);
+
+    if (ret < 0) {
+        ALOGE("Failed to recv binder genl data %d: %s", ret, strerror(errno));
+        return ret;
+    } else if (msg->nlh.nlmsg_type == NLMSG_ERROR) {
+        ret = msg->error;
+        ALOGE("Error msg received %d: %s ", ret, strerror(-ret));
+        return ret;
+    } else if (!NLMSG_OK(&msg->nlh, (__u32) ret)) {
+        ALOGE("Wrong binder genl message data");
+        return -EFAULT;
+    }
+
+    return 0;
+}
+
+int BinderGenl::getFamilyId() {
+    int ret;
+
+    ALOGV("Sending CTRL_CMD_GETFAMILY");
+    ret = send(GENL_ID_CTRL, CTRL_CMD_GETFAMILY, CTRL_ATTR_FAMILY_NAME, name, strlen(name) + 1);
+    if (ret < 0) {
+        ALOGE("Failed to send CTRL_CMD_GETFAMILY");
+        close();
+        return -1;
+    }
+
+    struct GenlMsg msg;
+    if (recv(&msg, sizeof(msg)) < 0) {
+        ALOGE("Failed to recv reply of CTRL_CMD_GETFAMILY");
+    }
+
+    struct nlmsghdr *nlh;
+    struct genlmsghdr *glh;
+    struct nlattr *nla;
+    nlh = &msg.nlh;
+    glh = (struct genlmsghdr *)NLMSG_DATA(nlh);
+    if (glh->cmd != CTRL_CMD_NEWFAMILY) {
+        ALOGE("Wrong glh cmd %d, expect %d", glh->cmd, CTRL_CMD_NEWFAMILY);
+        return -1;
+    }
+
+    int cur = 0;
+    int payload = GENLMSG_PAYLOAD(nlh);
+    while (cur < payload) {
+        ALOGV("Checking NLA payload %d / %d", cur, payload);
+        nla = (struct nlattr *)((char *)GENLMSG_DATA(glh) + cur);
+        ALOGV("NLA type / len: %d / %d", nla->nla_type, nla->nla_len);
+        cur += NLA_ALIGN(nla->nla_len);
+        if (nla->nla_type == CTRL_ATTR_FAMILY_ID) {
+            id = *(__u16*)NLA_DATA(nla);
+            ALOGV("Family id of [%s] is %d", name, id);
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+int BinderGenl::setReport(__u32 pid, __u32 flags) {
+    int ret;
+
+    ALOGV("Sending BINDER_GENL_CMD_SET %u %u", pid, flags);
+    ret = send2(id, BINDER_GENL_CMD_SET, BINDER_GENL_A_CMD_PID, &pid, sizeof(pid),
+                BINDER_GENL_A_CMD_FLAGS, &flags, sizeof(flags));
+    if (ret < 0) {
+        ALOGE("Failed to send BINDER_GENL_CMD_SET");
+        return -1;
+    }
+
+    struct GenlMsg msg;
+    if (recv(&msg, sizeof(msg)) < 0) {
+        ALOGE("Failed to recv reply of BINDER_GENL_CMD_SET");
+        return -1;
+    }
+
+    struct nlmsghdr *nlh;
+    struct genlmsghdr *glh;
+    struct nlattr *nla;
+    __u32 *p;
+    nlh = &msg.nlh;
+    glh = (struct genlmsghdr *)NLMSG_DATA(nlh);
+    if (glh->cmd != BINDER_GENL_CMD_REPLY) {
+        ALOGE("Wrong glh cmd %d, expect %d", glh->cmd, BINDER_GENL_CMD_REPLY);
+        return -1;
+    }
+
+    nla = (struct nlattr *)GENLMSG_DATA(glh);
+    if (nla->nla_type != BINDER_GENL_A_CMD_PID) {
+        ALOGE("Wrong nla type %d, expect %d", nla->nla_type, BINDER_GENL_A_CMD_PID);
+        return -1;
+    }
+    p = (__u32 *)NLA_DATA(nla);
+    if (*p != pid) {
+        ALOGE("Wrong pid %d, expect %d", *p, pid);
+        return -1;
+    }
+
+    nla = NLA_NEXT(nla);
+    if (nla->nla_type != BINDER_GENL_A_CMD_FLAGS) {
+        ALOGE("Wrong nla type %d, expect %d", nla->nla_type, BINDER_GENL_A_CMD_FLAGS);
+        return -1;
+    }
+    p = (__u32 *)NLA_DATA(nla);
+    if (*p != flags) {
+        ALOGE("Wrong pid %d, expect %d", *p, flags);
+        return -1;
+    }
+
+    ALOGD("[%s] genl report flags[%u]: %u", name, pid, flags);
+
+    return 0;
+}
+
+int BinderGenl::getReport(__u32 report[__BINDER_GENL_A_REPORT_MAX]) {
+    struct GenlMsg msg;
+    if (recv(&msg, sizeof(msg)) < 0) {
+        ALOGE("Failed to recv binder report");
+    }
+
+    struct nlmsghdr *nlh;
+    struct genlmsghdr *glh;
+    struct nlattr *nla;
+    nlh = &msg.nlh;
+    glh = (struct genlmsghdr *)NLMSG_DATA(nlh);
+    if (glh->cmd != BINDER_GENL_CMD_REPORT) {
+        ALOGE("Wrong glh cmd %d, expect %d", glh->cmd, BINDER_GENL_CMD_REPORT);
+        return -1;
+    }
+
+    int attr = 0;
+    int cur = 0;
+    int payload = GENLMSG_PAYLOAD(nlh);
+    while (cur < payload) {
+        ALOGV("Checking NLA payload %d / %d", cur, payload);
+        nla = (struct nlattr *)((char *)GENLMSG_DATA(glh) + cur);
+        ALOGV("NLA type / len: %d / %d", nla->nla_type, nla->nla_len);
+        cur += NLA_ALIGN(nla->nla_len);
+        if (nla->nla_type <= 0 || nla->nla_type > BINDER_GENL_A_REPORT_MAX) {
+            ALOGE("Invalid NLA type: %d", nla->nla_type);
+            return -1;
+        }
+        report[nla->nla_type] = *(__u32*)NLA_DATA(nla);
+        ALOGV("report[%d] = %u", nla->nla_type, report[nla->nla_type]);
+        attr++;
+    }
+
+    if (attr != BINDER_GENL_A_REPORT_MAX) {
+        ALOGE("Wrong NLAs %d, expect %d", attr, BINDER_GENL_A_REPORT_MAX);
+        return -1;
+    }
+
+    return 0;
+}
+
+} // namespace android
---
+++ libs/binder/include/binder/BinderGenl.h
@@ -0,0 +1,264 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+/*
+ * https://r.android.com/2852650 made kernel uapi linux/socket.h depend on
+ * bits/sockaddr_storage, which is outside of the kernel uapi directory,
+ * breaking the build. Before a better solution is found, copy the right
+ * version of kernel uapi linux/socket.h here.
+ *
+ * TODO: remove this workaround after the kernel header files are fixed.
+ */
+
+/* Start of kernel uapi <linux/socket.h> */
+#ifndef _UAPI_LINUX_SOCKET_H
+#define _UAPI_LINUX_SOCKET_H
+
+/*
+ * Desired design of maximum size and alignment (see RFC2553)
+ */
+#define _K_SS_MAXSIZE	128	/* Implementation specific max size */
+
+typedef unsigned short __kernel_sa_family_t;
+
+/*
+ * The definition uses anonymous union and struct in order to control the
+ * default alignment.
+ */
+struct __kernel_sockaddr_storage {
+	union {
+		struct {
+			__kernel_sa_family_t	ss_family; /* address family */
+			/* Following field(s) are implementation specific */
+			char __data[_K_SS_MAXSIZE - sizeof(unsigned short)];
+				/* space to achieve desired size, */
+				/* _SS_MAXSIZE value minus size of ss_family */
+		};
+		void *__align; /* implementation specific desired alignment */
+	};
+};
+/* End of kernel uapi <linux/socket.h> */
+
+#define SOCK_SNDBUF_LOCK	1
+#define SOCK_RCVBUF_LOCK	2
+
+#define SOCK_BUF_LOCK_MASK (SOCK_SNDBUF_LOCK | SOCK_RCVBUF_LOCK)
+
+#define SOCK_TXREHASH_DEFAULT	255
+#define SOCK_TXREHASH_DISABLED	0
+#define SOCK_TXREHASH_ENABLED	1
+
+#endif /* _UAPI_LINUX_SOCKET_H */
+
+#include <asm/types.h>
+#include <binder/Common.h>
+#include <linux/android/binder.h>
+#include <linux/genetlink.h>
+#include <stddef.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <functional>
+
+// Copy of binder genl header in case include/uapi/linux/*.h is not new enough
+#ifndef _UAPI_LINUX_ANDROID_BINDER_GENL_H
+#define _UAPI_LINUX_ANDROID_BINDER_GENL_H
+
+#define BINDER_GENL_FAMILY_NAME		"binder_genl"
+#define BINDER_GENL_FAMILY_VERSION	1
+
+enum binder_genl_flag {
+  BINDER_GENL_FLAG_FAILED = 1,
+  BINDER_GENL_FLAG_DELAYED = 2,
+  BINDER_GENL_FLAG_SPAM = 4,
+  BINDER_GENL_FLAG_OVERRIDE = 8,
+};
+
+enum {
+  BINDER_GENL_A_CMD_PID = 1,
+  BINDER_GENL_A_CMD_FLAGS,
+
+  __BINDER_GENL_A_CMD_MAX,
+  BINDER_GENL_A_CMD_MAX = (__BINDER_GENL_A_CMD_MAX - 1)
+};
+
+enum {
+  BINDER_GENL_A_REPORT_ERR = 1,
+  BINDER_GENL_A_REPORT_FROM_PID,
+  BINDER_GENL_A_REPORT_FROM_TID,
+  BINDER_GENL_A_REPORT_TO_PID,
+  BINDER_GENL_A_REPORT_TO_TID,
+  BINDER_GENL_A_REPORT_REPLY,
+  BINDER_GENL_A_REPORT_FLAGS,
+  BINDER_GENL_A_REPORT_CODE,
+  BINDER_GENL_A_REPORT_DATA_SIZE,
+
+  __BINDER_GENL_A_REPORT_MAX,
+  BINDER_GENL_A_REPORT_MAX = (__BINDER_GENL_A_REPORT_MAX - 1)
+};
+
+enum {
+  BINDER_GENL_CMD_SET = 1,
+  BINDER_GENL_CMD_REPLY,
+  BINDER_GENL_CMD_REPORT,
+
+  __BINDER_GENL_CMD_MAX,
+  BINDER_GENL_CMD_MAX = (__BINDER_GENL_CMD_MAX - 1)
+};
+
+#endif /* _UAPI_LINUX_ANDROID_BINDER_GENL_H */
+
+#define BINDER_GENL_MSG_SIZE 1024
+
+#define GENLMSG_DATA(glh) ((void *)((char *)(glh) + GENL_HDRLEN))
+#define GENLMSG_PAYLOAD(nlh) (NLMSG_PAYLOAD(nlh, 0) - GENL_HDRLEN)
+#define NLA_DATA(nla) ((void *)((char *)(nla) + NLA_HDRLEN))
+#define NLA_NEXT(nla) ((struct nlattr *)((char *)nla + NLA_ALIGN(nla->nla_len)))
+
+namespace android {
+
+/*
+ * The standard skeleton data struct for generic netlink messages
+ */
+struct GenlMsg {
+    struct nlmsghdr nlh;
+    union {
+        struct genlmsghdr glh;
+        int error;
+    };
+    char buf[BINDER_GENL_MSG_SIZE];
+};
+
+class BinderGenl;
+
+/**
+ * The netlink primitive functions talking to kernel binder genl driver.
+ */
+class BinderGenl {
+private:
+
+    /**
+     * The generic netlink family name
+     */
+    char name[GENL_NAMSIZ];
+
+    /**
+     * The netlink socket
+     */
+    int fd;
+
+    /**
+     * The pid of the current process
+     */
+     __u32 pid;
+
+    /**
+     * The generic netlink family ID
+     */
+    __u16 id;
+
+    /**
+     * Get the family id of binder generic netlink
+     *
+     * @return 0 on success, or -1 on error
+     */
+    int getFamilyId();
+
+public:
+    /**
+     * Constructor
+     *
+     * @param str The family name of this binder genl context
+     */
+    BinderGenl(const char* str) {
+        strncpy(name, str, GENL_NAMSIZ);
+    }
+
+    /**
+     * Open the generic netlink socket and initialize it
+     *
+     * @return    The socket, or <0 on error
+     */
+    LIBBINDER_EXPORTED int open();
+
+    /**
+     * Cleanup the generic netlink socket
+     */
+    LIBBINDER_EXPORTED void close();
+
+    /**
+     * Send data to the generic netlink socket.
+     *
+     * @param nlmsgType Message content type
+     * @param cmd       Generic netlink command
+     * @param nlaType   Attribute type
+     * @param nlaData   Attribute data
+     * @param nlaLen    Attribute length
+     * @return          The number of bytes sent, or <0 on error.
+     */
+    LIBBINDER_EXPORTED int send(__u16 nlmsgType, __u8 cmd, __u16 nlaType,
+                                void* nlaData, __u16 nlaLen);
+
+    /**
+     * Similar to send() but accept 2 NLAs
+     *
+     * @param nlmsgType Message content type
+     * @param cmd       Generic netlink command
+     * @param nlaType   Attribute type 1
+     * @param nlaData   Attribute data 1
+     * @param nlaLen    Attribute length 1
+     * @param nlaType2  Attribute type 2
+     * @param nlaData2  Attribute data 2
+     * @param nlaLen2   Attribute length 2
+     * @return          The number of bytes sent, or <0 on error.
+     */
+    LIBBINDER_EXPORTED int send2(__u16 nlmsgType, __u8 cmd, __u16 nlaType,
+                                 void* nlaData, __u16 nlaLen, __u16 nlaType2,
+                                 void* nlaData2, __u16 nlaLen2);
+
+    /**
+     * Receive data from the generic netlink socket.
+     *
+     * @param msg Message content
+     * @param len Message length
+     * @return    0 on success, or <0 on failure
+     */
+     LIBBINDER_EXPORTED int recv(struct GenlMsg* msg, int len);
+
+    /**
+     * Set what kinds of binder transactions are to be reported
+     *
+     * If pid is 0, the flags are applied to the whole binder context.
+     * Otherwise, the flags are applied to the specific process only.
+     *
+     * @param pid   The target process to set the flags
+     * @param flags The flags to be set (see enum binder_report_flag)
+     * @return 0 on success, or -1 on error
+     */
+    LIBBINDER_EXPORTED int setReport(__u32 pid, __u32 flags);
+
+    /**
+     * Receive next binder genl report.
+     *
+     * @param report The u32 array to store the binder genl report
+     * @return 0 on success, or -1 on error
+     */
+    LIBBINDER_EXPORTED int getReport(__u32 report[__BINDER_GENL_A_REPORT_MAX]);
+};
+
+} // namespace android
