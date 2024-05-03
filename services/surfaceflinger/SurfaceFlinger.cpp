f57f32a11b815dd900c1d8656f3d03e9d76bee0d
Jeff Xu <jeffxu@google.com>
2024-05-03 18:01:15 +0000

    code lab:
    Test: manual test
    
    Change-Id: Iefe991e105820c6972dc2e364d4e04260da8499b

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2736,6 +2736,10 @@ CompositeResultsPerDisplay SurfaceFlinger::composite(
         mDrawingState.colorMatrixChanged = false;
     }
 
+    refreshArgs.colorTransformMatrix =
+            mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                 vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
+
     refreshArgs.devOptForceClientComposition = mDebugDisableHWC;
 
     if (mDebugFlashDelay != 0) {
@@ -8158,13 +8162,15 @@ ftl::SharedFuture<FenceResult> SurfaceFlinger::renderScreenImpl(
                                         .isProtected = isProtected});
 
         const float colorSaturation = grayscale ? 0 : 1;
-        compositionengine::CompositionRefreshArgs refreshArgs{
-                .outputs = {output},
-                .layers = std::move(layerFEs),
-                .updatingOutputGeometryThisFrame = true,
-                .updatingGeometryThisFrame = true,
-                .colorTransformMatrix = calculateColorMatrix(colorSaturation),
-        };
+        compositionengine::CompositionRefreshArgs
+                refreshArgs{.outputs = {output},
+                            .layers = std::move(layerFEs),
+                            .updatingOutputGeometryThisFrame = true,
+                            .updatingGeometryThisFrame = true,
+                            .colorTransformMatrix = mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f},
+                                                         vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                                                         vec4{0.0f, 0.0f, -1.0f, 0.0f},
+                                                         vec4{0.0f, 1.0f, 1.0f, 1.0f})};
         compositionEngine->present(refreshArgs);
 
         return output->getRenderSurface()->getClientTargetAcquireFence();
