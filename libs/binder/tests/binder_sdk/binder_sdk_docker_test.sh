2d387f80d4e1b87dd1e74e3abbea128f93cbe9c9
Tomasz Wasilczyk <twasilczyk@google.com>
2024-05-09 14:36:19 -0700

    Implement binder_sdk_docker_test(s)
    These tests attempt to build binder_sdk snapshot in controlled
    environment outside of Android build tree.
    
    Bug: 302718225
    Test: atest binder_sdk_docker_test_clang
    Test: atest binder_sdk_docker_test_gcc
    Test: atest binder_sdk_docker_test_gnumake
    Test: cd frameworks/native/libs/binder/tests/binder_sdk
          ./binder_sdk_docker_test.sh clang.Dockerfile
    Change-Id: I11fdd05428726884541fbb61dea34e4e82389f4f

---
+++ libs/binder/tests/binder_sdk/binder_sdk_docker_test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -ex
+
+TEST_NAME="$(basename "$0")"
+DOCKER_TAG="${TEST_NAME}-${RANDOM}${RANDOM}"
+DOCKER_FILE=*.Dockerfile
+DOCKER_RUN_FLAGS=
+
+# Guess if we're running as an Android test or directly
+if [ "$(ls -1 ${DOCKER_FILE} | wc -l)" == "1" ]; then
+    # likely running as `atest binder_sdk_docker_test_XYZ`
+    DOCKER_PATH="$(dirname $(readlink --canonicalize --no-newline binder_sdk.zip))"
+else
+    # likely running directly as `./binder_sdk_docker_test.sh` - provide mode for easy testing
+    RED='\033[1;31m'
+    NO_COLOR='\033[0m'
+
+    DOCKER_RUN_FLAGS="--interactive --tty"
+
+    DOCKER_FILE="$1"
+    if [ ! -f "${DOCKER_FILE}" ]; then
+        echo -e "${RED}Docker file '${DOCKER_FILE}' doesn't exist. Please provide one as an argument.${NO_COLOR}"
+        exit 1
+    fi
+
+    if [ ! -d "${ANDROID_BUILD_TOP}" ]; then
+        echo -e "${RED}ANDROID_BUILD_TOP doesn't exist. Please lunch some target.${NO_COLOR}"
+        exit 1
+    fi
+    ${ANDROID_BUILD_TOP}/build/soong/soong_ui.bash --make-mode binder_sdk
+    BINDER_SDK_ZIP="${ANDROID_BUILD_TOP}/out/soong/.intermediates/frameworks/native/libs/binder/binder_sdk/*/binder_sdk.zip"
+    DOCKER_PATH="$(dirname $(ls -1 ${BINDER_SDK_ZIP} | head --lines=1))"
+fi
+
+function cleanup {
+    docker rmi --force "${DOCKER_TAG}"
+}
+trap cleanup EXIT
+
+docker build --tag "${DOCKER_TAG}" --file ${DOCKER_FILE} ${DOCKER_PATH}
+docker run ${DOCKER_RUN_FLAGS} --rm "${DOCKER_TAG}"
