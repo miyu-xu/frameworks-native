62854cebb9325054a9bd2cbcef255f60f8d609d6
Nathanael Robbins <nejrobbins@gmail.com>
2024-05-11 06:18:58 +0000

    Android codelab change Test: manual atest
    Change-Id: I2b6f1ec5995faf0c3dedb8cf1ac7c63a6cb2d650

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2737,6 +2737,9 @@ CompositeResultsPerDisplay SurfaceFlinger::composite(
     }
 
     refreshArgs.devOptForceClientComposition = mDebugDisableHWC;
+    refreshArgs.colorTransformMatrix =
+            mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                 vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
 
     if (mDebugFlashDelay != 0) {
         refreshArgs.devOptForceClientComposition = true;
@@ -8158,13 +8161,16 @@ ftl::SharedFuture<FenceResult> SurfaceFlinger::renderScreenImpl(
                                         .isProtected = isProtected});
 
         const float colorSaturation = grayscale ? 0 : 1;
-        compositionengine::CompositionRefreshArgs refreshArgs{
-                .outputs = {output},
-                .layers = std::move(layerFEs),
-                .updatingOutputGeometryThisFrame = true,
-                .updatingGeometryThisFrame = true,
-                .colorTransformMatrix = calculateColorMatrix(colorSaturation),
-        };
+        compositionengine::CompositionRefreshArgs
+                refreshArgs{.outputs = {output},
+                            .layers = std::move(layerFEs),
+                            .updatingOutputGeometryThisFrame = true,
+                            .updatingGeometryThisFrame = true,
+                            .colorTransformMatrix = mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f},
+                                                         vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                                                         vec4{0.0f, 0.0f, -1.0f, 0.0f},
+                                                         vec4{0.0f, 1.0f, 1.0f, 1.0f})};
+        updateColorMatrixLocked();
         compositionEngine->present(refreshArgs);
 
         return output->getRenderSurface()->getClientTargetAcquireFence();
