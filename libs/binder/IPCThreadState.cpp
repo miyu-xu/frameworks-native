dd81169a67e8c65c64691e8766fd2fc84113a858
Tomasz Wasilczyk <twasilczyk@google.com>
2024-05-13 16:10:28 -0700

    Enable Kernel Binder for Binder SDK
    Bug: 341997808
    Test: cd frameworks/native/libs/binder/tests/binder_sdk
          ./binder_sdk_docker_test.sh clang.Dockerfile
    Change-Id: Ie78137f980ccf8f91ade5b0794c56597df44e791

--- libs/binder/IPCThreadState.cpp
+++ libs/binder/IPCThreadState.cpp
@@ -22,10 +22,6 @@
 #include <binder/BpBinder.h>
 #include <binder/TextOutput.h>
 
-#include <cutils/sched_policy.h>
-#include <utils/CallStack.h>
-#include <utils/Log.h>
-
 #include <atomic>
 #include <errno.h>
 #include <inttypes.h>
@@ -838,8 +834,9 @@ status_t IPCThreadState::transact(int32_t handle,
         if (mCallRestriction != ProcessState::CallRestriction::NONE) [[unlikely]] {
             if (mCallRestriction == ProcessState::CallRestriction::ERROR_IF_NOT_ONEWAY) {
                 ALOGE("Process making non-oneway call (code: %u) but is restricted.", code);
-                CallStack::logStack("non-oneway call", CallStack::getCurrent(10).get(),
-                    ANDROID_LOG_ERROR);
+                //                CallStack::logStack("non-oneway call",
+                //                CallStack::getCurrent(10).get(),
+                //                    ANDROID_LOG_ERROR);
             } else /* FATAL_IF_NOT_ONEWAY */ {
                 LOG_ALWAYS_FATAL("Process may not make non-oneway calls (code: %u).", code);
             }
@@ -1009,8 +1006,8 @@ status_t IPCThreadState::waitForResponse(Parcel *reply, status_t *acquireResult)
         switch (cmd) {
         case BR_ONEWAY_SPAM_SUSPECT:
             ALOGE("Process seems to be sending too many oneway calls.");
-            CallStack::logStack("oneway spamming", CallStack::getCurrent().get(),
-                    ANDROID_LOG_ERROR);
+            //            CallStack::logStack("oneway spamming", CallStack::getCurrent().get(),
+            //                    ANDROID_LOG_ERROR);
             [[fallthrough]];
         case BR_TRANSACTION_COMPLETE:
             if (!reply && !acquireResult) goto finish;
@@ -1147,7 +1144,7 @@ status_t IPCThreadState::talkWithDriver(bool doReceive)
             std::string message = logStream.str();
             ALOGI("%s", message.c_str());
         }
-#if defined(__ANDROID__)
+#if defined(__ANDROID__) || 1
         if (ioctl(mProcess->mDriverFD, BINDER_WRITE_READ, &bwr) >= 0)
             err = NO_ERROR;
         else
