40e95d6bf102516e7efd9f750732255bde5cbfa8
Tomasz Wasilczyk <twasilczyk@google.com>
2024-05-13 16:10:28 -0700

    Enable Kernel Binder for Binder SDK
    Bug: 341997808
    Test: cd frameworks/native/libs/binder/tests/binder_sdk
          ./binder_sdk_docker_test.sh clang.Dockerfile
    Change-Id: Ie78137f980ccf8f91ade5b0794c56597df44e791

---
+++ libs/binder/tests/binder_sdk/run_all_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+
+BUILD_DIR=$1
+SERVICEMANAGER_DIR=${BUILD_DIR}/frameworks/native/cmds/servicemanager
+BINDER_DIR=${BUILD_DIR}/frameworks/native/libs/binder
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NO_COLOR='\033[0m'
+
+if [ ! -d "${BINDER_DIR}" ]; then
+    echo -e "${RED}Invalid build dir. Please provide it as the first argument.${NO_COLOR}"
+    exit -1
+fi
+
+${SERVICEMANAGER_DIR}/servicemanager &
+SERVICEMANAGER_PID=$!
+function cleanup {
+    kill $SERVICEMANAGER_PID
+}
+trap cleanup EXIT
+
+${BINDER_DIR}/tests/binderClearBufTest
+${BINDER_DIR}/tests/binderDriverInterfaceTestSdk
+${BINDER_DIR}/tests/binderParcelBenchmark
+${BINDER_DIR}/tests/binderUnitTest
+${BINDER_DIR}/tests/binderLibTestSdk
+${BINDER_DIR}/tests/binderStabilityTestSdk
+${BINDER_DIR}/tests/binderRpcWireProtocolTest
+${BINDER_DIR}/ndk/tests/libbinder_ndk_unit_testSdk
+${BINDER_DIR}/tests/binderRpcTest
+# Skipped for time sake
+#${BINDER_DIR}/tests/binderRpcTestNoKernel
+#${BINDER_DIR}/tests/binderRpcTestSingleThreaded
+#${BINDER_DIR}/tests/binderRpcTestSingleThreadedNoKernel
+
+echo -e "${GREEN}All tests succeeded${NO_COLOR}"
