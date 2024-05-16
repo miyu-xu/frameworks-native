07dc1a121044e31fa552c2eced72a76ba5ddf572
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-05-16 21:51:22 +0000

    Revert "Fix typo."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/f7524a28-c179-4e47-936a-d6507719c35b). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Bug: 341141037
    Change-Id: If9c65c43b859fea88a956b0560c6ac67854b3c70
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- libs/binder/tests/binderSafeInterfaceTest.cpp
+++ libs/binder/tests/binderSafeInterfaceTest.cpp
@@ -723,7 +723,7 @@ TEST_F(SafeInterfaceTest, TestIncrementNoCopyNoMove) {
     ASSERT_EQ(a.getValue() + 1, aPlusOne.getValue());
 }
 
-TEST_F(SafeInterfaceTest, TestIncrementParcelableVector) {
+TEST_F(SafeInterfaceTest, TestIncremementParcelableVector) {
     const std::vector<TestParcelable> a{TestParcelable{1}, TestParcelable{2}};
     std::vector<TestParcelable> aPlusOne;
     status_t result = mSafeInterfaceTest->increment(a, &aPlusOne);
