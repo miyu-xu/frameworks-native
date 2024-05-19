8a752f7e350d58acf165cc08f42efa3b0f433ce3
Parth Sane <parthsane@google.com>
2024-05-19 13:02:07 +0000

    Add perfetto traces to ServiceManager
    Test: N/A
    Bug: N/A
    Change-Id: Ida65d96241899f9f50a48f4b2f12ec1f7a5dc978

--- cmds/servicemanager/main.cpp
+++ cmds/servicemanager/main.cpp
@@ -22,9 +22,25 @@
 #include <sys/timerfd.h>
 #include <utils/Looper.h>
 #include <utils/StrongPointer.h>
+#include "perfetto/public/abi/track_event_abi.h"
+#include "perfetto/public/producer.h"
+#include "perfetto/public/te_category_macros.h"
+#include "perfetto/public/te_macros.h"
+#include "perfetto/public/track_event.h"
 
 #include "Access.h"
 #include "ServiceManager.h"
+
+namespace android {
+
+void register_perfetto_te_categories() {
+    struct PerfettoProducerInitArgs perfetto_args = {0};
+    perfetto_args.backends = PERFETTO_BACKEND_SYSTEM;
+    PerfettoProducerInit(perfetto_args);
+    PerfettoTeInit();
+    PERFETTO_TE_REGISTER_CATEGORIES(PERFETTO_SM_CATEGORIES);
+}
+} // namespace android
 
 using ::android::Access;
 using ::android::IPCThreadState;
@@ -131,6 +147,8 @@ int main(int argc, char** argv) {
     }
 
     const char* driver = argc == 2 ? argv[1] : "/dev/binder";
+
+    android::register_perfetto_te_categories();
 
     LOG(INFO) << "Starting sm instance on " << driver;
 
