69b9359eb114e506f397534072a2ccd2e2aa3ccd
qinyige1 <qinyige1@xiaomi.com>
2024-05-22 09:49:02 +0800

    [Stability][XIAOMI][Feature] Avoid duplicated file in bugreport.zip.
    BUGOS2-4424
    
    Based on feature support of ziparchive.
    
    Signed-off-by: qinyige1 <qinyige1@xiaomi.com>
    Change-Id: Ie33e3cddcfc85464d9601e8d7769738266f2b3e1

--- cmds/dumpstate/dumpstate.cpp
+++ cmds/dumpstate/dumpstate.cpp
@@ -860,9 +860,16 @@ status_t Dumpstate::AddZipEntryFromFd(const std::string& entry_name, int fd,
 
     // Logging statement  below is useful to time how long each entry takes, but it's too verbose.
     // MYLOGD("Adding zip entry %s\n", entry_name.c_str());
-    size_t flags = ZipWriter::kCompress | ZipWriter::kDefaultCompression;
+    size_t flags = ZipWriter::kCompress | ZipWriter::kDefaultCompression
+                   | ZipWriter::kFailOnDupEntry;
     int32_t err = zip_writer_->StartEntryWithTime(valid_name.c_str(), flags,
                                                   get_mtime(fd, ds.now_));
+    if (err == kDuplicatedEntry) {
+        // Ignore duplicate entry.
+        MYLOGD("Ignore duplicated entry: %s\n", valid_name.c_str());
+        return OK;
+    }
+
     if (err != 0) {
         MYLOGE("zip_writer_->StartEntryWithTime(%s): %s\n", valid_name.c_str(),
                ZipWriter::ErrorCodeString(err));
