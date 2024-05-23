5670289f755e716932e2a73ba5fdf493bcb319ff
Srija Mukhopadhyay <srijamukh@google.com>
2024-05-23 07:00:55 +0000

    Android codelab change Test: manual atest
    Change-Id: I5a3c8ff647f04d3d817549b3053d1254aed71715

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2736,6 +2736,10 @@ CompositeResultsPerDisplay SurfaceFlinger::composite(
         mDrawingState.colorMatrixChanged = false;
     }
 
+    refreshArgs.colorTransformMatrix =
+            mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                 vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
+
     refreshArgs.devOptForceClientComposition = mDebugDisableHWC;
 
     if (mDebugFlashDelay != 0) {
@@ -8158,13 +8162,26 @@ ftl::SharedFuture<FenceResult> SurfaceFlinger::renderScreenImpl(
                                         .isProtected = isProtected});
 
         const float colorSaturation = grayscale ? 0 : 1;
+        //        compositionengine::CompositionRefreshArgs refreshArgs{
+        //                .outputs = {output},
+        //                .layers = std::move(layerFEs),
+        //                .updatingOutputGeometryThisFrame = true,
+        //                .updatingGeometryThisFrame = true,
+        //                .colorTransformMatrix = calculateColorMatrix(colorSaturation),
+        //        };
+
         compositionengine::CompositionRefreshArgs refreshArgs{
                 .outputs = {output},
                 .layers = std::move(layerFEs),
                 .updatingOutputGeometryThisFrame = true,
                 .updatingGeometryThisFrame = true,
-                .colorTransformMatrix = calculateColorMatrix(colorSaturation),
+                .colorTransformMatrix =
+                        mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                             vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f}),
         };
+
+        base::StringPrintf("%.2fadb", colorSaturation);
+
         compositionEngine->present(refreshArgs);
 
         return output->getRenderSurface()->getClientTargetAcquireFence();
