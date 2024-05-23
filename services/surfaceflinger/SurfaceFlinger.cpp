36213974bd5554af9b754cfb211c6cf072ec1ab9
qinyige1 <qinyige1@xiaomi.com>
2024-05-23 15:59:08 +0800

    Adapt addClientLayer to LayerHierarchy.
    Method addClientLayer still traverses mDrawingState, which is not
    adapted after LayerLifecycleManager enabled. Dump info through
    LayerHierarchyBuilder.
    
    Bug: 341641452
    Test: Manual
    Change-Id: I781696548138dfcb1ec3e463a32ae5b399f10eac

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -4452,12 +4452,36 @@ bool SurfaceFlinger::latchBuffers() {
     return !mLayersWithQueuedFrames.empty() && newDataLatched;
 }
 
+void SurfaceFlinger::logSampledLayers() const {
+    static_cast<void>(mScheduler->schedule([&] {
+        ALOGE("Dumping on-screen layers.");
+        mLayerHierarchyBuilder.dumpLayerSample(mLayerHierarchyBuilder.getHierarchy());
+        ALOGE("Dumping off-screen layers.");
+        mLayerHierarchyBuilder.dumpLayerSample(mLayerHierarchyBuilder.getOffscreenHierarchy());
+    }));
+}
+
 status_t SurfaceFlinger::addClientLayer(LayerCreationArgs& args, const sp<IBinder>& handle,
                                         const sp<Layer>& layer, const wp<Layer>& parent,
                                         uint32_t* outTransformHint) {
     if (mNumLayers >= MAX_LAYERS) {
+        static std::atomic<nsecs_t> lasttime{0};
+        nsecs_t now = systemTime();
+        if (lasttime != 0 && ns2s(now - lasttime.load()) < 10) {
+            ALOGE("AddClientLayer already dumped 10s before");
+            return NO_MEMORY;
+        } else {
+            lasttime = now;
+        }
+
         ALOGE("AddClientLayer failed, mNumLayers (%zu) >= MAX_LAYERS (%zu)", mNumLayers.load(),
               MAX_LAYERS);
+
+        if (mLayerLifecycleManagerEnabled) {
+            logSampledLayers();
+            return NO_MEMORY;
+        }
+
         static_cast<void>(mScheduler->schedule([=] {
             ALOGE("Dumping layer keeping > 20 children alive:");
             bool leakingParentLayerFound = false;
