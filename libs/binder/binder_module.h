6bcf38c61c8767283c9b9938c4445109d08e7ab1
Tomasz Wasilczyk <twasilczyk@google.com>
2024-06-14 16:04:20 -0700

    Fix build against older Kernel
    Bug: 341997808
    Test: mma
    Test: aosp/3081709
    Change-Id: Ib7c08c7ff5b789d571c922091578335a2b90b59e

--- libs/binder/binder_module.h
+++ libs/binder/binder_module.h
@@ -32,4 +32,41 @@
 #include <linux/android/binder.h>
 #include <sys/ioctl.h>
 
+#if defined(ANDROID) || defined(__BIONIC__)
+#define HAS_EXTENDED_ERROR 1
+#define HAS_FLAT_BINDER_FLAG_INHERIT_RT 1
+#define HAS_FLAT_BINDER_FLAG_SCHED_POLICY 1
+#define HAS_FREEZE_INFO 1
+#else
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 13, 0)
+enum {
+    BR_FROZEN_REPLY = _IO('r', 18),
+    BR_ONEWAY_SPAM_SUSPECT = _IO('r', 19),
+    BINDER_ENABLE_ONEWAY_SPAM_DETECTION = _IOW('b', 16, __u32),
+};
+#define HAS_FREEZE_INFO 0
+#else
+#define HAS_FREEZE_INFO 1
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
+#define HAS_EXTENDED_ERROR 0
+#else
+#define HAS_EXTENDED_ERROR 1
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
+enum { BR_TRANSACTION_PENDING_FROZEN = _IO('r', 20) };
+#endif
+enum flat_binder_object_shifts {
+    FLAT_BINDER_FLAG_SCHED_POLICY_SHIFT = 9,
+};
+
+// INHERIT_RT and SCHED_POLICY is only in Android common kernel and hasn't been pushed to upstream.
+#define HAS_FLAT_BINDER_FLAG_INHERIT_RT 0
+#define HAS_FLAT_BINDER_FLAG_SCHED_POLICY 0
+#endif // defined(ANDROID) || defined(__BIONIC__)
+
 #endif // _BINDER_MODULE_H_
