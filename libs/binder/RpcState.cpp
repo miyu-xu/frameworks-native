5726ab56f5823ac6f3f741c44b0e72d958e48d43
Tomasz Wasilczyk <twasilczyk@google.com>
2024-06-25 14:25:50 -0700

    Fix some memcheck nits on cmake build
    Bug: 341997808
    Test: mma
    Test: manual build with valgrind
    Change-Id: Ic649146f5731faa46cd879502cb05045814229bf

--- libs/binder/RpcState.cpp
+++ libs/binder/RpcState.cpp
@@ -44,8 +44,8 @@ using android::binder::unique_fd;
 
 #if RPC_FLAKE_PRONE
 void rpcMaybeWaitToFlake() {
-    [[clang::no_destroy]] static std::random_device r;
-    [[clang::no_destroy]] static RpcMutex m;
+    [[no_destroy]] static std::random_device r;
+    [[no_destroy]] static RpcMutex m;
     unsigned num;
     {
         RpcMutexLockGuard lock(m);
