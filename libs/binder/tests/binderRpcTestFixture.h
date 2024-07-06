c3246f10d84b44338a35d224f2fdf5a8dc2c19ef
Andrei Homescu <ahomescu@google.com>
2024-07-06 00:47:35 +0000

    libbinder: Enable file descriptor tests on Trusty
    Bug: 242940548
    Test: build.py --test com.android.trusty.binderRpcTest
    Change-Id: I67fe8daaa3c076d0fb5eff8e866e747df386d5c9

--- libs/binder/tests/binderRpcTestFixture.h
+++ libs/binder/tests/binderRpcTestFixture.h
@@ -136,14 +136,15 @@ public:
 
     // Whether the test params support sending FDs in parcels.
     bool supportsFdTransport() const {
-        if (socketType() == SocketType::TIPC) {
-            // Trusty does not support file descriptors yet
-            return false;
-        }
         return clientVersion() >= 1 && serverVersion() >= 1 && rpcSecurity() != RpcSecurity::TLS &&
                 (socketType() == SocketType::PRECONNECTED || socketType() == SocketType::UNIX ||
                  socketType() == SocketType::UNIX_BOOTSTRAP ||
-                 socketType() == SocketType::UNIX_RAW);
+                 socketType() == SocketType::UNIX_RAW || socketType() == SocketType::TIPC);
+    }
+
+    RpcSession::FileDescriptorTransportMode fdTransportMode() const {
+        return socketType() == SocketType::TIPC ? RpcSession::FileDescriptorTransportMode::TRUSTY
+                                                : RpcSession::FileDescriptorTransportMode::UNIX;
     }
 
     void SetUp() override {
