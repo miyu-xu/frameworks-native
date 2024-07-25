0b81ae5fd416a5dcb5d0393afc6fb76c00570969
Pawan Wagh <waghpawan@google.com>
2024-07-25 23:11:01 +0000

    Move tracing calls to libbinder_ndk
    gen_trace is broken for NDK backend. Moving tracing to libbinder_ndk
    and removing dependency on libandroid while enabling
    tracing in ndk. Adding an API to set funciton names from interface
    to be used by tracing function calls in libbinder_ndk.
    
    Flag : EXEMPT bugfix
    Test: m && acloud create --local-image
    Test: set gen_trace: true for NDK
    Bug: 349936395
    Change-Id: I76e8ec19ac7ef75668008fb7ec70e6c2c7af4404

--- libs/binder/ndk/include_cpp/android/binder_interface_utils.h
+++ libs/binder/ndk/include_cpp/android/binder_interface_utils.h
@@ -164,7 +164,8 @@ class ICInterface : public SharedRefBase {
      * Helper method to create a class
      */
     static inline AIBinder_Class* defineClass(const char* interfaceDescriptor,
-                                              AIBinder_Class_onTransact onTransact);
+                                              AIBinder_Class_onTransact onTransact,
+                                              const char** codeToFunction, size_t functionCount);
 
    private:
     class ICInterfaceData {
@@ -253,7 +254,8 @@ std::shared_ptr<ICInterface> ICInterface::asInterface(AIBinder* binder) {
 }
 
 AIBinder_Class* ICInterface::defineClass(const char* interfaceDescriptor,
-                                         AIBinder_Class_onTransact onTransact) {
+                                         AIBinder_Class_onTransact onTransact,
+                                         const char** codeToFunction, size_t functionCount) {
     AIBinder_Class* clazz = AIBinder_Class_define(interfaceDescriptor, ICInterfaceData::onCreate,
                                                   ICInterfaceData::onDestroy, onTransact);
     if (clazz == nullptr) {
@@ -272,6 +274,10 @@ AIBinder_Class* ICInterface::defineClass(const char* interfaceDescriptor,
         AIBinder_Class_setHandleShellCommand(clazz, ICInterfaceData::handleShellCommand);
     }
 #endif
+
+    if (__builtin_available(android 36, *)) {
+        AIBinder_Class_setCodeMap(clazz, codeToFunction, functionCount);
+    }
     return clazz;
 }
 
