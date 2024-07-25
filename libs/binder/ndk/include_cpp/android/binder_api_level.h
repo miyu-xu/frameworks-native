7d5ac7a152dc8c9aa43980bca308e1e3ca0aae46
Pawan Wagh <waghpawan@google.com>
2024-07-25 23:11:01 +0000

    Add trace APIs in NDK
    gen_trace is broken for NDK backend. Moving tracing to libbinder_ndk
    and removing dependency on libandroid while enabling
    tracing in ndk. Adding an API to set funciton names from interface
    to be used by tracing function calls in libbinder_ndk.
    
    Flag : EXEMPT bugfix
    Test: m && acloud create --local-image
    Test: set gen_trace: true for NDK
    Bug: 349936395
    Change-Id: I76e8ec19ac7ef75668008fb7ec70e6c2c7af4404

---
+++ libs/binder/ndk/include_cpp/android/binder_api_level.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <sys/cdefs.h>
+
+// Include llndk-versioning.h only for vendor build as it is not available for NDK headers.
+#if defined(__ANDROID_VENDOR__)
+#include <android/llndk-versioning.h>
+#elif !defined(API_LEVEL_AT_LEAST)
+#if defined(__BIONIC__)
+#define API_LEVEL_AT_LEAST(sdk_api_level, vendor_api_level) \
+    (__builtin_available(android sdk_api_level, *))
+#else
+#define API_LEVEL_AT_LEAST(sdk_api_level, vendor_api_level) (true)
+#endif  // __BIONIC__
+#endif  // __ANDROID_VENDOR__
