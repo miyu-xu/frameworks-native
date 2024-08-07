d90a65886b592352a2b9db286642ba9264888169
Parth Sane <parthsane@google.com>
2024-08-07 16:25:39 +0000

    Aidl: Add bool to enable Client Side Cache
    Test: atest binderUnitTest
    Bug: 333854840
    Change-Id: Id9b6e32e4fc074d4a72d4835dc8aeb9620c29fc8

--- libs/binder/IServiceManager.cpp
+++ libs/binder/IServiceManager.cpp
@@ -143,7 +143,7 @@ protected:
     virtual Status realGetService(const std::string& name, sp<IBinder>* _aidl_return) {
         Service service;
         Status status = mUnifiedServiceManager->getService2(name, &service);
-        *_aidl_return = service.get<Service::Tag::binder>();
+        *_aidl_return = service.get<Service::Tag::serviceWithCacheInfo>()->service;
         return status;
     }
 };
@@ -330,12 +330,17 @@ sp<IBinder> CppBackendShim::getService(const String16& name) const {
 }
 
 sp<IBinder> CppBackendShim::checkService(const String16& name) const {
-    Service ret;
-    if (!mUnifiedServiceManager->checkService(String8(name).c_str(), &ret).isOk()) {
-        return nullptr;
+    {
+        Service service;
+        if (!mUnifiedServiceManager->checkService(String8(name).c_str(), &service).isOk()) {
+            return nullptr;
+        }
+        auto ret = service.get<Service::Tag::serviceWithCacheInfo>();
+        if (!ret.has_value()) {
+            return nullptr;
+        }
+        return ret->service;
     }
-    return ret.get<Service::Tag::binder>();
-}
 
 status_t CppBackendShim::addService(const String16& name, const sp<IBinder>& service,
                                     bool allowIsolated, int dumpsysPriority) {
