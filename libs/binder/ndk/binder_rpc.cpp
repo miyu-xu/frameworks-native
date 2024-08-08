794760b7c76913f248aaa2e39ca1dbbfaaca4818
Devin Moore <devinmoore@google.com>
2024-08-08 21:18:39 +0000

    other stuff - to be broken up into smaller CLs
    Test: atest binderRpcTest
    
    Change-Id: I46d853a4f4342257b5432ecd0ece073b2d355275

---
+++ libs/binder/ndk/binder_rpc.cpp
@@ -0,0 +1,266 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android/binder_rpc.h>
+#include <binder/IServiceManager.h>
+#include <linux/vm_sockets.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include <variant>
+
+#include "ibinder_internal.h"
+#include "status_internal.h"
+
+using ::android::defaultServiceManager;
+using ::android::IBinder;
+using ::android::IServiceManager;
+using ::android::sp;
+using ::android::String16;
+using ::android::String8;
+using ::android::binder::Status;
+
+struct ARpc_ConnectionInfo {
+    // TODO support more connection types
+    std::variant<sockaddr_vm, sockaddr_un> addr;
+};
+
+struct ARpc_Accessor final : public ::android::RefBase {
+    static ARpc_Accessor* make(const char* instance, sp<IBinder> binder) {
+        LOG_ALWAYS_FATAL_IF(binder == nullptr, "ARpc_Accessor requires a non-null binder");
+        android::status_t status = android::validateAccessor(String16(instance), binder);
+        if (status == android::OK) {
+            return new ARpc_Accessor(binder);
+        } else {
+            ALOGE("The given binder is not a valid IAccessor for %s. Status: %s", instance,
+                  android::statusToString(status).c_str());
+            return nullptr;
+        }
+    }
+
+    sp<IBinder> asBinder() { return mAccessorBinder; }
+
+    ~ARpc_Accessor() { ALOGI("ARpc_Accessor dtor"); }
+
+   private:
+    ARpc_Accessor(sp<IBinder> accessor) : mAccessorBinder(accessor) {}
+    ARpc_Accessor() = delete;
+    sp<IBinder> mAccessorBinder;
+};
+
+struct ARpc_AccessorProvider {
+   public:
+    static ARpc_AccessorProvider* make(std::weak_ptr<android::AccessorProvider> cookie,
+                                       void* data) {
+        if (!cookie.expired()) {
+            return new ARpc_AccessorProvider(cookie, data);
+        }
+        ALOGE("Null AccessorProvider cookie from libbinder");
+        return nullptr;
+    }
+    std::weak_ptr<android::AccessorProvider> mProviderCookie;
+    void* mData;
+
+   private:
+    ARpc_AccessorProvider() = delete;
+
+    ARpc_AccessorProvider(std::weak_ptr<android::AccessorProvider> provider, void* data)
+        : mProviderCookie(provider), mData(data) {}
+};
+
+ARpc_AccessorProvider* ARpc_addAccessorProvider(ARpc_AccessorProvider_getAccessor provider,
+                                                void* data,
+                                                ARpc_AccessorProviderUserData_delete onDelete) {
+    if (provider == nullptr) {
+        ALOGE("Null provider passed to ARpc_addAccessorProvider_getAccessor");
+        return nullptr;
+    }
+    if (data && onDelete == nullptr) {
+        ALOGE("If a non-null data ptr is passed to ARpc_addAccessorProvider, then a "
+              "ARpc_AccessorProviderUserData_delete callback must alse be passed to delete "
+              "the data object once the ARpc_AccessorProvider is removed.");
+        return nullptr;
+    }
+    std::function<sp<IBinder>(const String16& name)> generate =
+            [provider, data](const String16& name) -> sp<IBinder> {
+        ARpc_Accessor* accessor = provider(String8(name).c_str(), data);
+        if (accessor == nullptr) {
+            ALOGI("The supplied ARpc_AccessorProvider_getAccessor returned nullptr");
+            return nullptr;
+        }
+        sp<IBinder> binder = accessor->asBinder();
+        ARpc_Accessor_delete(accessor);
+        return binder;
+    };
+    std::function<void()> onDeleteCb;
+    if (onDelete) {
+        onDeleteCb = [data, onDelete] { onDelete(data); };
+    }
+
+    std::weak_ptr<android::AccessorProvider> cookie =
+            android::addAccessorProvider(std::move(generate), std::move(onDeleteCb), data);
+    return ARpc_AccessorProvider::make(cookie, data);
+}
+
+binder_status_t ARpc_removeAccessorProvider(ARpc_AccessorProvider* provider) {
+    if (provider == nullptr) {
+        ALOGE("Attempting to remove a null ARpc_AccessorProvider");
+        return STATUS_UNEXPECTED_NULL;
+    }
+
+    android::status_t status =
+            android::removeAccessorProvider(provider->mProviderCookie, provider->mData);
+    delete provider;
+    return PruneStatusT(status);
+}
+
+ARpc_Accessor* ARpc_Accessor_new(const char* instance, ARpc_ConnectionInfoProvider provider,
+                                 void* data, ARpc_ConnectionInfoProviderUserData_delete onDelete) {
+    if (instance == nullptr) {
+        ALOGE("Instance argument must be valid when calling ARpc_Accessor_new");
+        return nullptr;
+    }
+    if (data && onDelete == nullptr) {
+        ALOGE("If a non-null data ptr is passed to ARpc_Accessor_new, then a "
+              "ARpc_ConnectionInfoProviderUserData_delete callback must alse be passed to delete "
+              "the data object once the ARpc_Accessor is deleted.");
+        return nullptr;
+    }
+    android::RpcSocketAddressProvider generate;
+    if (provider) {
+        generate = [provider, data](const String16& name, sockaddr* outAddr, size_t addrLen) {
+            ARpc_ConnectionInfo* info = provider(String8(name).c_str(), data);
+            if (info == nullptr) {
+                ALOGI("The supplied ARpc_ConnectionInfoProvider returned nullptr");
+                return;
+            }
+            if (auto addr = std::get_if<sockaddr_vm>(&info->addr)) {
+                LOG_ALWAYS_FATAL_IF(addr->svm_family != AF_VSOCK,
+                                    "ARpc_ConnectionInfo invalid family");
+                if (addrLen < sizeof(sockaddr_vm)) {
+                    ALOGE("Provided outAddr is too small! Expecting %zu, got %zu",
+                          sizeof(sockaddr_vm), addrLen);
+                    ARpc_ConnectionInfo_delete(info);
+                    return;
+                }
+                ALOGI("Connection info provider found AF_VSOCK. family %d, port %d, cid %d",
+                      addr->svm_family, addr->svm_port, addr->svm_cid);
+                *outAddr = *reinterpret_cast<sockaddr*>(addr);
+            } else if (auto addr = std::get_if<sockaddr_un>(&info->addr)) {
+                LOG_ALWAYS_FATAL_IF(addr->sun_family != AF_UNIX,
+                                    "ARpc_ConnectionInfo invalid family");
+                if (addrLen < sizeof(sockaddr_un)) {
+                    ALOGE("Provided outAddr is too small! Expecting %zu, got %zu",
+                          sizeof(sockaddr_un), addrLen);
+                    ARpc_ConnectionInfo_delete(info);
+                    return;
+                }
+                *outAddr = *reinterpret_cast<sockaddr*>(addr);
+            } else {
+                ALOGE("Unsupported address family type when trying to get ARpcConnection info");
+            }
+            ARpc_ConnectionInfo_delete(info);
+        };
+    } else {
+        ALOGE("Can't create a new ARpc_Accessor without a ARpc_ConnectionInfoProvider and it is "
+              "null");
+        return nullptr;
+    }
+    std::function<void()> onDeleteCb;
+    if (onDelete) {
+        onDeleteCb = [data, onDelete] { onDelete(data); };
+    }
+    sp<IBinder> accessorBinder =
+            android::createAccessor(String16(instance), std::move(generate), std::move(onDeleteCb));
+    if (accessorBinder) {
+        ALOGE("service manager found an accessor, so returning one now from _new");
+        return ARpc_Accessor::make(instance, accessorBinder);
+    } else {
+        ALOGE("service manager did not get us an accessor");
+        return nullptr;
+    }
+}
+
+void ARpc_Accessor_delete(ARpc_Accessor* accessor) {
+    delete accessor;
+}
+
+AIBinder* ARpc_Accessor_asBinder(ARpc_Accessor* accessor) {
+    if (!accessor) {
+        ALOGE("ARpc_Accessor argument is null.");
+        return nullptr;
+    }
+
+    sp<IBinder> binder = accessor->asBinder();
+    sp<AIBinder> aBinder = ABpBinder::lookupOrCreateFromBinder(binder);
+    AIBinder* ptr = aBinder.get();
+    if (ptr) {
+        ptr->incStrong(nullptr);
+    } else {
+        ALOGE("Failed to lookupOrCreateFromBinder");
+    }
+    return ptr;
+}
+
+ARpc_Accessor* ARpc_Accessor_fromBinder(const char* instance, AIBinder* binder) {
+    if (!binder) {
+        ALOGE("binder argument is null");
+        return nullptr;
+    }
+    if (binder->asABBinder()) {
+        ALOGE("Binder is local ABBinder and can not be used for ARpc_Accessor_fromBinder");
+        return nullptr;
+    }
+    sp<IBinder> accessorBinder = binder->getBinder();
+    if (accessorBinder) {
+        return ARpc_Accessor::make(instance, accessorBinder);
+    } else {
+        ALOGE("Attempting to get an ARpc_Accessor for %s but AIBinder::getBinder returned null",
+              instance);
+        return nullptr;
+    }
+}
+
+ARpc_ConnectionInfo* ARpc_ConnectionInfo_new(const sockaddr* addr, socklen_t len) {
+    if (addr == nullptr || len < 0 || static_cast<size_t>(len) < sizeof(sa_family_t)) {
+        ALOGE("Invalid arguments in Arpc_Connection_new");
+        return nullptr;
+    }
+
+    if (addr->sa_family == AF_VSOCK) {
+        if (len != sizeof(sockaddr_vm)) {
+            // socklen_t was int32_t on 32-bit and uint32_t on 64 bit. I don't
+            // trust it... so also check against SIZE_MAX just in case.
+            size_t socklen = len < 0 || static_cast<uintmax_t>(len) > SIZE_MAX ? 0 : len;
+            ALOGE("Incorrect size of %zu for AF_VSOCK sockaddr_vm. Expecting %zu", socklen,
+                  sizeof(sockaddr_vm));
+            return nullptr;
+        }
+        sockaddr_vm vm = *reinterpret_cast<const sockaddr_vm*>(addr);
+        ALOGI("Arpc_ConnectionInfo_new found AF_VSOCK. family %d, port %d, cid %d", vm.svm_family,
+              vm.svm_port, vm.svm_cid);
+
+        return new ARpc_ConnectionInfo(*reinterpret_cast<const sockaddr_vm*>(addr));
+    }
+
+    ALOGE("ARpc APIs only support AF_VSOCK right now but the supplied sockadder::sa_family is: %hu",
+          addr->sa_family);
+    return nullptr;
+}
+
+void ARpc_ConnectionInfo_delete(ARpc_ConnectionInfo* info) {
+    delete info;
+}
