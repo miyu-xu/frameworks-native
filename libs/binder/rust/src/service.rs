b3f5c9a54b6d4ffb12810fbaa75c45737f28a27c
Devin Moore <devinmoore@google.com>
2024-08-09 23:20:03 +0000

    Add a libbinder_rs wrapper around the new ARpc APIs
    This allows rust clients to set up client connections to binder RPC
    services underneath the libbinder_rs service manager APIs.
    
    Test: atest binderRpcTest vm_accessor_test
    Bug: 358427181
    Change-Id: I05f1c5ff4e3447262eaf9df8250c9d99e3414559

--- libs/binder/rust/src/service.rs
+++ libs/binder/rust/src/service.rs
@@ -23,6 +23,14 @@ use std::ffi::{c_void, CStr, CString};
 use std::os::raw::c_char;
 use std::sync::Mutex;
 
+#[cfg(not(any(android_vendor, android_vndk)))]
+use {
+    libc::sockaddr,
+    nix::sys::socket::{SockaddrLike, UnixAddr, VsockAddr},
+    std::ptr,
+    std::sync::Arc,
+};
+
 /// Register a new service with the default service manager.
 ///
 /// Registers the given binder object with the given identifier. If successful,
@@ -244,4 +252,135 @@ pub fn get_declared_instances(interface: &str) -> Result<Vec<String>> {
             eprintln!("An interface instance name was not a valid UTF-8 string: {}", e);
             StatusCode::BAD_VALUE
         })
+}
+
+/// Rust wrapper around ABinderRpc_Accessor objects for RPC binder service management.
+///
+/// Dropping the `Accessor` will drop the underlying object and the binder it owns.
+#[cfg(not(any(android_vendor, android_vndk)))]
+pub struct Accessor {
+    accessor: *mut sys::ABinderRpc_Accessor,
+}
+
+/// Socket connection info required for libbinder to connect to a service.
+#[cfg(not(any(android_vendor, android_vndk)))]
+pub enum ConnectionInfo {
+    /// For vsock connection
+    Vsock(VsockAddr),
+    /// For unix domain socket connection
+    Unix(UnixAddr),
+}
+
+#[cfg(not(any(android_vendor, android_vndk)))]
+/// Safety: A `Accessor` is a wrapper around `ABinderRpc_Accessor` and
+/// a pointer to a `Fn` which is `Sync` and `Send` (the cookie field). As
+/// `ABinderRpc_Accessor` is threadsafe, this structure is too.
+unsafe impl Send for Accessor {}
+
+#[cfg(not(any(android_vendor, android_vndk)))]
+/// Safety: A `Accessor` is a wrapper around `AIBinder_Accessor` and
+/// a pointer to a `Fn` which is `Sync` and `Send` (the cookie field). As
+/// `ABinderRpc_Accessor` is threadsafe, this structure is too.
+unsafe impl Sync for Accessor {}
+
+#[cfg(not(any(android_vendor, android_vndk)))]
+impl Accessor {
+    /// Create a new accessor that will call the given callback when its
+    /// connection info is required.
+    pub fn new<F>(instance: &str, callback: F) -> Accessor
+    where
+        F: Fn() -> Option<ConnectionInfo> + Send + Sync + 'static,
+    {
+        let callback: *mut c_void = Arc::into_raw(Arc::new(callback)) as *mut c_void;
+        let inst = CString::new(instance).unwrap();
+
+        // Safety: The function pointer is a valid connection_info callback.
+        // This call returns an owned `ABinderRpc_Accessor` pointer which
+        // must be destroyed via `ABinderRpc_Accessor_delete` when no longer
+        // needed.
+        // When the underlying ABinderRpc_Accessor is deleted, it will call
+        // the cookie_decr_refcount callback to release its strong ref.
+        let accessor = unsafe {
+            sys::ABinderRpc_Accessor_new(
+                inst.as_ptr(),
+                Some(Self::connection_info::<F>),
+                callback,
+                Some(Self::cookie_decr_refcount::<F>),
+            )
+        };
+
+        Accessor { accessor }
+    }
+
+    /// Get the underlying binder for this Accessor for when it needs to be either
+    /// registered with service manager or sent to another process.
+    pub fn as_binder(&self) -> Option<SpIBinder> {
+        // Safety: `ABinderRpc_Accessor_asBinder` returns either a null pointer or a
+        // valid pointer to an owned `AIBinder`. Either of these values is safe to
+        // pass to `SpIBinder::from_raw`.
+        unsafe { SpIBinder::from_raw(sys::ABinderRpc_Accessor_asBinder(self.accessor)) }
+    }
+
+    /// Callback invoked from C++ when the connection info is needed.
+    ///
+    /// # Safety
+    ///
+    /// The `cookie` parameter must be the cookie for an `Arc<F>` and
+    /// the caller must hold a ref-count to it.
+    unsafe extern "C" fn connection_info<F>(
+        _instance: *const c_char,
+        cookie: *mut c_void,
+    ) -> *mut binder_ndk_sys::ABinderRpc_ConnectionInfo
+    where
+        F: Fn() -> Option<ConnectionInfo> + Send + Sync + 'static,
+    {
+        if cookie.is_null() {
+            return ptr::null_mut();
+        }
+        // Safety: The caller promises that `cookie` is for an Arc<F>.
+        let callback = unsafe { (cookie as *const F).as_ref().unwrap() };
+
+        let connection = callback().unwrap();
+        match connection {
+            ConnectionInfo::Vsock(addr) => {
+                // Safety: The sockaddr is being copied in the NDK API
+                unsafe { sys::ABinderRpc_ConnectionInfo_new(addr.as_ptr(), addr.len()) }
+            }
+            ConnectionInfo::Unix(addr) => {
+                // Safety: The sockaddr is being copied in the NDK API
+                // The cast is from sockaddr_un* to sockaddr*.
+                unsafe {
+                    sys::ABinderRpc_ConnectionInfo_new(addr.as_ptr() as *const sockaddr, addr.len())
+                }
+            }
+        }
+    }
+
+    /// Callback that decrements the ref-count.
+    /// This is invoked from C++ when a binder is unlinked.
+    ///
+    /// # Safety
+    ///
+    /// The `cookie` parameter must be the cookie for an `Arc<F>` and
+    /// the owner must give up a ref-count to it.
+    unsafe extern "C" fn cookie_decr_refcount<F>(cookie: *mut c_void)
+    where
+        F: Fn() -> Option<ConnectionInfo> + Send + Sync + 'static,
+    {
+        // Safety: The caller promises that `cookie` is for an Arc<F>.
+        unsafe { Arc::decrement_strong_count(cookie as *const F) };
+    }
+}
+
+#[cfg(not(any(android_vendor, android_vndk)))]
+impl Drop for Accessor {
+    fn drop(&mut self) {
+        // Safety: `self.accessor` is always a valid, owned
+        // `ABinderRpc_Accessor` pointer returned by
+        // `ABinderRpc_Accessor_new` when `self` was created. This delete
+        // method can only be called once when `self` is dropped.
+        unsafe {
+            sys::ABinderRpc_Accessor_delete(self.accessor);
+        }
+    }
 }
