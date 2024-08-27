cdab34c11c7bb24283b5351073cdd0e07329b5db
Shuangxi Xiang <xiangshuangxi@xiaomi.corp-partner.google.com>
2024-08-27 07:28:20 +0000

    Remove the redundant parameters in applyTransactionsLocked
    In the major Android version upgrade from 13 to 14, the addCommittedTransactions function in the applyTransactionsLocked function that requires the vsyncId parameter has been moved to the updateLayerSnapshots and updateLayerSnapshotsLegacy functions. The rest of the operations in the applyTransactionsLocked function no longer require the vsyncId parameter, so the declaration and definition information about vsyncId in the applyTransactionsLocked function can be removed here.
    
    Bug：362415544
    
    Change-Id: Ic4ded171e90bcfb7a0d58df2c272f4c219be3a04
    Merged-In: Ic4ded171e90bcfb7a0d58df2c272f4c219be3a04
    Signed-off-by: xiangshuangxi@xiaomi.corp-partner.google.com

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -814,9 +814,9 @@ private:
             REQUIRES(mStateLock, kMainThreadContext);
     // Flush pending transactions that were presented after desiredPresentTime.
     // For test only
-    bool flushTransactionQueues(VsyncId) REQUIRES(kMainThreadContext);
+    bool flushTransactionQueues() REQUIRES(kMainThreadContext);
 
-    bool applyTransactions(std::vector<TransactionState>&, VsyncId) REQUIRES(kMainThreadContext);
+    bool applyTransactions(std::vector<TransactionState>&) REQUIRES(kMainThreadContext);
     bool applyAndCommitDisplayTransactionStatesLocked(std::vector<TransactionState>& transactions)
             REQUIRES(kMainThreadContext, mStateLock);
 
@@ -854,8 +854,8 @@ private:
 
     static LatchUnsignaledConfig getLatchUnsignaledConfig();
     bool shouldLatchUnsignaled(const layer_state_t&, size_t numStates, bool firstTransaction) const;
-    bool applyTransactionsLocked(std::vector<TransactionState>& transactions, VsyncId)
-            REQUIRES(mStateLock, kMainThreadContext);
+    bool applyTransactionsLocked(std::vector<TransactionState>& transactions)
+            REQUIRES(mStateLock);
     uint32_t setDisplayStateLocked(const DisplayState& s) REQUIRES(mStateLock);
     uint32_t addInputWindowCommands(const InputWindowCommands& inputWindowCommands)
             REQUIRES(mStateLock);
