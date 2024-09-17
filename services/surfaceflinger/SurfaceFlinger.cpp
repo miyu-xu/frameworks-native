222b1d8a8fa12c92615aa6968e1962a64de9142f
Simon (Qiong) Sun <sqsun@google.com>
2024-09-17 18:21:13 +0000

    Android codelab change Test: manual atest
    Change-Id: Ibf25182244f26fbf3ef3957f5ff3a45b5f35c254

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2817,7 +2817,9 @@ CompositeResultsPerDisplay SurfaceFlinger::composite(
     refreshArgs.internalDisplayRotationFlags = getActiveDisplayRotationFlags();
 
     if (CC_UNLIKELY(mDrawingState.colorMatrixChanged)) {
-        refreshArgs.colorTransformMatrix = mDrawingState.colorMatrix;
+        refreshArgs.colorTransformMatrix =
+                mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                     vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
         mDrawingState.colorMatrixChanged = false;
     }
 
@@ -8581,7 +8583,9 @@ ftl::SharedFuture<FenceResult> SurfaceFlinger::renderScreenImpl(
                 .layers = std::move(layerFEs),
                 .updatingOutputGeometryThisFrame = true,
                 .updatingGeometryThisFrame = true,
-                .colorTransformMatrix = calculateColorMatrix(colorSaturation),
+                .colorTransformMatrix =
+                        mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                             vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f}),
         };
         compositionEngine->present(refreshArgs);
 
