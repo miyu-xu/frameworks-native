5e483098ad85539eb6683eb1298e0a15f5ca907e
Parth Sane <parthsane@google.com>
2024-10-04 14:14:07 +0000

    Add Tracing for Binder Caching
    Test: perfetto -c <perfetto config>
    Bug: 333854840
    Change-Id: Iffb1e00f94722f60c85305e0596ce58b0df5a6da

--- libs/binder/BackendUnifiedServiceManager.h
+++ libs/binder/BackendUnifiedServiceManager.h
@@ -18,6 +18,7 @@
 #include <android/os/BnServiceManager.h>
 #include <android/os/IServiceManager.h>
 #include <binder/IPCThreadState.h>
+#include <binder/Trace.h>
 #include <map>
 #include <memory>
 
@@ -59,6 +60,8 @@ public:
     }
 
     bool removeItem(const std::string& key, const sp<IBinder>& who) {
+        std::string trace_name = "BinderCacheWithInvalidation::removeItem " + key;
+        binder::ScopedTrace aidl_trace(ATRACE_TAG_AIDL, trace_name.c_str());
         std::lock_guard<std::mutex> lock(mCacheMutex);
         if (auto it = mCache.find(key); it != mCache.end()) {
             if (it->second.service == who) {
@@ -86,6 +89,8 @@ public:
                 return binder::Status::fromStatusT(status);
             }
         }
+        binder::ScopedTrace aidl_trace(ATRACE_TAG_AIDL,
+                                       "BinderCacheWithInvalidation::setItem Successfully Cached");
         std::lock_guard<std::mutex> lock(mCacheMutex);
         Entry entry = {.service = item, .deathRecipient = deathRecipient};
         mCache[key] = entry;
