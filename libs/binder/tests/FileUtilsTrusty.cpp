6f14dfea9bcbfc96d3873f75c583a38398f0f16f
Stephen Crane <cranes@google.com>
2024-10-29 01:22:38 +0000

    libbinder: Add Android to Trusty file handle tests
    Adds tests for passing file handles from Android to Trusty via the TIPC
    transport.
    
    Bug: 242940548
    Test: atest binderRpcToTrustyTest
    Change-Id: Id152854461f2247c8c0a88bb11ff0991dbc84fea

--- libs/binder/tests/FileUtilsTrusty.cpp
+++ libs/binder/tests/FileUtilsTrusty.cpp
@@ -17,12 +17,21 @@
 #include <binder/RpcThreads.h>
 #include <log/log.h>
 #include <sys/mman.h>
+#ifndef BINDER_RPC_TO_TRUSTY_TEST
 #include <trusty/memref.h>
+#else // BINDER_RPC_TO_TRUSTY_TEST
+#include <BufferAllocator/BufferAllocator.h>
+#include <binder/unique_fd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#endif
 
 #include "FileUtils.h"
 
 constexpr size_t kPageSize = 4096;
 
+#ifndef BINDER_RPC_TO_TRUSTY_TEST
 // We can only create memref's over page-aligned regions from
 // the heap or data section. We could use posix_memalign to allocate
 // these pages on demand, but we have no way to tell when they're not
@@ -32,6 +41,7 @@ constexpr size_t kPageRingBufferSize = 4;
 
 static __attribute__((aligned(kPageSize))) char sPageRingBuffer[kPageRingBufferSize][kPageSize];
 static size_t sPageRingBufferIdx = 0;
+#endif
 
 struct SizedString {
     uint64_t len;
@@ -40,6 +50,7 @@ struct SizedString {
 
 namespace android::binder {
 
+#ifdef BINDER_NO_LIBBASE
 bool ReadFdToString(borrowed_fd fd, std::string* content) {
     content->clear();
 
@@ -53,7 +64,9 @@ bool ReadFdToString(borrowed_fd fd, std::string* content) {
     LOG_ALWAYS_FATAL_IF(ret != 0, "Failed to unmap page");
     return true;
 }
+#endif
 
+#ifndef BINDER_RPC_TO_TRUSTY_TEST
 unique_fd mockFileDescriptor(std::string contents) {
     SizedString* str = reinterpret_cast<SizedString*>(sPageRingBuffer[sPageRingBufferIdx]);
     LOG_ALWAYS_FATAL_IF(contents.length() + sizeof(str->len) > kPageSize);
@@ -66,5 +79,30 @@ unique_fd mockFileDescriptor(std::string contents) {
     sPageRingBufferIdx = (sPageRingBufferIdx + 1) % kPageRingBufferSize;
     return unique_fd{memref};
 }
+#else  // BINDER_RPC_TO_TRUSTY_TEST
+// Create a DmaBuf allocated FD to send to Trusty that returns `contents` when
+// read. The buffer format must match the format expected by ReadFdToString.
+unique_fd mockFileDescriptor(std::string contents) {
+    SizedString* str = nullptr;
+    LOG_ALWAYS_FATAL_IF(contents.length() + sizeof(str->len) > kPageSize);
+
+    BufferAllocator allocator;
+
+    int ret = allocator.Alloc(kDmabufSystemHeapName, kPageSize, 0, 0 /* legacy align */);
+    LOG_ALWAYS_FATAL_IF(ret < 0, "Could not allocate heap");
+    unique_fd fd(ret);
+
+    char* buf = (char*)mmap(0, kPageSize, PROT_WRITE, MAP_SHARED, fd.get(), 0);
+    LOG_ALWAYS_FATAL_IF(buf == MAP_FAILED, "Could not map dmabuf");
+    str = reinterpret_cast<SizedString*>(buf);
+    str->len = contents.length();
+    contents.copy(str->chars, str->len);
+    ret = munmap(buf, kPageSize);
+    int savedErrno = errno;
+    LOG_ALWAYS_FATAL_IF(ret != 0, "Could not unmap buffer: %s", strerror(savedErrno));
+
+    return fd;
+}
+#endif // BINDER_RPC_TO_TRUSTY_TEST
 
 } // namespace android::binder
