715303a9d972eb7990d46e1eb266ab858ecd6958
sm1021.shin <sm1021.shin@samsung.com>
2024-10-29 10:49:35 +0900

    libvk: wa set max_buffer_count 64
    Change-Id: I9a1bbf0e9d9b7b9cf527d9fcbd0ce29bb2227328
    Signed-off-by: sm1021.shin <sm1021.shin@samsung.com>

--- vulkan/libvulkan/swapchain.cpp
+++ vulkan/libvulkan/swapchain.cpp
@@ -968,6 +968,10 @@ VkResult GetPhysicalDeviceSurfaceCapabilities2KHR(
 
         err = window->query(window, NATIVE_WINDOW_MAX_BUFFER_COUNT,
                             &max_buffer_count);
+	if (max_buffer_count == 0)
+	{
+	    max_buffer_count = 64;
+	}
         if (err != android::OK) {
             ALOGE("NATIVE_WINDOW_MAX_BUFFER_COUNT query failed: %s (%d)",
                   strerror(-err), err);
