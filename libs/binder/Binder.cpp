01bd848e88e18e56b61fb349e330209ef6d99cfb
Parth Sane <parthsane@google.com>
2025-02-12 08:19:14 -0800

    Spam Detection - Collect and store binder spam data
    Change-Id: I9be17c337ea198f3f7d8423714b00fba64208b9f

--- libs/binder/Binder.cpp
+++ libs/binder/Binder.cpp
@@ -18,6 +18,7 @@
 
 #include <atomic>
 #include <set>
+#include <string>
 
 #include <binder/BpBinder.h>
 #include <binder/IInterface.h>
@@ -53,10 +54,10 @@ constexpr uid_t kUidRoot = 0;
 // in prebuilts.
 #ifdef __LP64__
 static_assert(sizeof(IBinder) == 24);
-static_assert(sizeof(BBinder) == 40);
+static_assert(sizeof(BBinder) == 56);
 #else
 static_assert(sizeof(IBinder) == 12);
-static_assert(sizeof(BBinder) == 20);
+static_assert(sizeof(BBinder) == 24);
 #endif
 
 // global b/c b/230079120 - consistent symbol table
@@ -405,12 +406,17 @@ status_t BBinder::transact(
             break;
         }
         default:
-            std::string name = "BBinder: potential binder observation";
-
+            std::string transactionName = getTransactionName(code);
             binder::ScopedTrace aidlTrace(ATRACE_TAG_AIDL,
-                                          (name + " " + getTransactionName(code)).c_str());
+                                          (name + " " + transactionName).c_str());
+            CallSession callSession;
+            if (mObserver) {
+                callSession = mObserver->onCallStarted();
+            }
             err = onTransact(code, data, reply, flags);
-            break;
+            if (mObserver) {
+                mObserver->onCallEnded(callSession, code, transactionName, IPCThreadState::self()->getCallingUid(), data.dataSize(), reply ? reply->dataSize() : 0, err != NO_ERROR);
+            }
     }
 
     // In case this is being transacted on in the same process.
@@ -823,7 +829,7 @@ status_t BBinder::onTransact(
     }
 }
 
-const std::string BBinder::getTransactionName(int transactionCode) {
+const std::string BBinder::getTransactionName(uint32_t transactionCode) {
     return std::to_string(transactionCode);
 }
 
