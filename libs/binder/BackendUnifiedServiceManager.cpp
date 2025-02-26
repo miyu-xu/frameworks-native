66b8422b1e1acf288096cb8083ce337a8d7d6299
Parth Sane <parthsane@google.com>
2025-02-26 03:08:40 -0800

    Remove log if cache is disabled.
    Change-Id: I8d419df56e22b7a334f419a58724d4144efc86f3
    Flag: Bugfix
    Bug: 399202756
    Test: N/A

--- libs/binder/BackendUnifiedServiceManager.cpp
+++ libs/binder/BackendUnifiedServiceManager.cpp
@@ -130,10 +130,9 @@ os::ServiceWithMetadata createServiceWithMetadata(const sp<IBinder>& service, bo
 
 bool BinderCacheWithInvalidation::isClientSideCachingEnabled(const std::string& serviceName) {
     sp<ProcessState> self = ProcessState::selfOrNull();
+    // Should not cache if process state could not be found, or if thread pool
+    // max could is not greater than zero.
     if (!self || self->getThreadPoolMaxTotalThreadCount() <= 0) {
-        ALOGW("Thread Pool max thread count is 0. Cannot cache binder as linkToDeath cannot be "
-              "implemented. serviceName: %s",
-              serviceName.c_str());
         return false;
     }
     if (kRemoveStaticList) return true;
