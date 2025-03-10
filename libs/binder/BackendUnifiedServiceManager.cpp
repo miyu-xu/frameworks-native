5dcb4209bd6b8bcec60be7361768d54efd1a9a88
Devin Moore <devinmoore@google.com>
2025-03-10 17:32:29 +0000

    IServiceManager add checkServiceAccess to delegate sepolicy check
    Android Virtualization Framework is in charge of VSOCK communications on
    Android devices. Specifically, virtmgr manages these connection for AVF
    clients that own VMs. It is forked from the owner and can get the
    selinux SID of that owning process.
    We want to check that the owning process has access to services that it
    is requesting.
    So we provide a new checkServiceAccess method that takes the SID of the
    owner and checks if it has access to "find" a given service before
    virtmgr provides access to it.
    
    Test: atest vm_accessor_test
    Bug: 358427181
    Change-Id: Ie389af62f1af541a554294d79f72b4c3b65c3e45

--- libs/binder/BackendUnifiedServiceManager.cpp
+++ libs/binder/BackendUnifiedServiceManager.cpp
@@ -467,6 +467,17 @@ Status BackendUnifiedServiceManager::getServiceDebugInfo(
                                      kUnsupportedOpNoServiceManager);
 }
 
+Status BackendUnifiedServiceManager::checkServiceAccess(
+        const AidlServiceManager::CallerContext& callerCtx, const std::string& service,
+        const std::string& permission, bool* _aidl_return) {
+    if (mTheRealServiceManager) {
+        return mTheRealServiceManager->checkServiceAccess(callerCtx, service, permission,
+                                                          _aidl_return);
+    }
+    return Status::fromExceptionCode(Status::EX_UNSUPPORTED_OPERATION,
+                                     kUnsupportedOpNoServiceManager);
+}
+
 [[clang::no_destroy]] static std::once_flag gUSmOnce;
 [[clang::no_destroy]] static sp<BackendUnifiedServiceManager> gUnifiedServiceManager;
 
--- libs/binder/servicedispatcher.cpp
+++ libs/binder/servicedispatcher.cpp
@@ -190,6 +190,12 @@ public:
             std::vector<android::os::ServiceDebugInfo>* _aidl_return) override {
         return mImpl->getServiceDebugInfo(_aidl_return);
     }
+    android::binder::Status checkServiceAccess(
+            const android::os::IServiceManager::CallerContext& callerCtx,
+            const std::string& service, const std::string& permission,
+            bool* _aidl_return) override {
+        return mImpl->checkServiceAccess(callerCtx, service, permission, _aidl_return);
+    }
 
 private:
     sp<android::os::IServiceManager> mImpl;
