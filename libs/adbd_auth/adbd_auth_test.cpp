ee1295a7ca5e495ce83ec0f16a1d9ef6f83425b7
Fabien Sanglard <sanglardf@google.com>
2025-03-13 14:16:59 -0700

    Add capability to send tls server port
    Test: NA
    Fixes: 402927103
    Change-Id: I8106727715882ae348132fb18a8333ac216b9315

---
+++ libs/adbd_auth/adbd_auth_test.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "adbd_auth.h"
+
+class AdbAuthTest: public ::testing::Test {
+   public:
+    static void SetUpTestCase() {
+
+    }
+    static void TearDownTestCase() {
+
+    }
+
+    void SetUp() {
+
+    }
+
+    void TearDown() {
+
+    }
+};
+
+TEST_F(AdbAuthTest, SendTcpPort) {
+  ASSERT_TRUE(1 == 1);
+}
+
+TEST(x, f) {
+  ASSERT_FALSE(true);
+}
