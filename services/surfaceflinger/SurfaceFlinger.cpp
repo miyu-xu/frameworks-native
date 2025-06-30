b93a7c4590b573680c02f60387e2c6f7559f8ce0
hehwang <hehwang@marusys.com>
2025-06-30 14:00:18 +0900

    Android codelab change Test: manual atest
    Change-Id: I514eb10a719e3e6827cb8f75b2f2c408ef19563a

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -6498,6 +6498,8 @@ mat4 SurfaceFlinger::calculateColorMatrix(float saturation) {
 }
 
 void SurfaceFlinger::updateColorMatrixLocked() {
+    mClientColorMatrix = mat4(vec4{1.0f, 0.0f, 0.0f, 0.0f}, vec4{0.0f, -1.0f, 0.0f, 0.0f},
+                         vec4{0.0f, 0.0f, -1.0f, 0.0f}, vec4{0.0f, 1.0f, 1.0f, 1.0f});
     mat4 colorMatrix =
             mClientColorMatrix * calculateColorMatrix(mGlobalSaturationFactor) * mDaltonizer();
 
