4346aec1b9923f55d371921bf5c69062f839fa17
Quanrui <liuquanrui@xiaomi.corp-partner.google.com>
2025-07-10 17:00:56 +0800

    Use DISPLAY_P3 as bestDataspace of P3_XRB.
    Change-Id: I27698f9886d87bb158bc5b32c26425a5f43a6ed2
    Signed-off-by: Quanrui <liuquanrui@xiaomi.corp-partner.google.com>

--- services/surfaceflinger/CompositionEngine/src/Output.cpp
+++ services/surfaceflinger/CompositionEngine/src/Output.cpp
@@ -70,6 +70,9 @@ using CompositionStrategyPredictionState =
         OutputCompositionState::CompositionStrategyPredictionState;
 namespace {
 
+static constexpr auto P3_XRB = static_cast<ui::Dataspace>(
+    ui::Dataspace::STANDARD_DCI_P3 | ui::Dataspace::TRANSFER_SRGB | ui::Dataspace::RANGE_EXTENDED);
+
 template <typename T>
 class Reversed {
 public:
@@ -1024,6 +1027,7 @@ ui::Dataspace Output::getBestDataspace(ui::Dataspace* outHdrDataSpace,
             case ui::Dataspace::DISPLAY_BT2020:
                 bestDataSpace = ui::Dataspace::DISPLAY_BT2020;
                 break;
+            case P3_XRB:
             case ui::Dataspace::DISPLAY_P3:
                 bestDataSpace = ui::Dataspace::DISPLAY_P3;
                 break;
